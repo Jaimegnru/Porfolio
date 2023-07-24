624df30a67419e866d3090dd3a09f180787c3a5c
Jaimegnru <139849158+Jaimegnru@users.noreply.github.com>
2023-07-24 11:22:44 +0200

    24/07 11:22

--- src/components/atoms/Footers/Footers.component.tsx
+++ src/components/atoms/Footers/Footers.component.tsx
@@ -3,7 +3,7 @@
 export const Footer = () => {
 
     return (
-        <footer className="fixed  bottom-0 flex gap-3 bg-sky-200 w-full overflow-auto h-20 border-t-2 border-sky-600 justify-center items-center">
+        <footer className="fixed  bottom-0 flex gap-3 bg-sky-200 w-full overflow-auto h-12 border-t-2 border-sky-600 justify-center items-center">
             <div className='flex w-full h-full sm:w-full justify-center items-center'>
                 <p className="text-sky-700 font-medium">Jaime Galán © 2023</p>
             </div>
